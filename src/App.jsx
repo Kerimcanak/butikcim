993fcc8979b1086f4a58cae6ee95f5dde176c69e
Kerim <kerimcanak@gmail.com>
2024-08-07 04:54:18 +0300

    rearranged the files to conform the design and navigation ease

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Shop from './pages/Shop';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={Shop} />
     </Router>
   );
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+
+function EmptyComponent() {
+  return (
+    <div>
+      <div className="bg-emerald-700 w-full h-16">
+      </div>
+
+    </div>
+  )
+}
+
+export default EmptyComponent
