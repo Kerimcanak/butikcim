ac95f59f8048a3ed5e44edff0684fa4cc55dd309
Kerim <kerimcanak@gmail.com>
2024-08-07 06:15:13 +0300

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -6,7 +6,8 @@ function EmptyComponent() {
       <div className="bg-emerald-700 w-full h-16">
       </div>
       <Header/>
-      <div className="bg-zinc-50 flex flex-col justify-around items-center p-4 gap-4 md:flex-row">  {[1, 2, 3, 4].map((num) => (
+      <div className="bg-zinc-50 flex flex-col justify-around items-center p-4 gap-4 md:flex-row">  
+      {[1, 2, 3, 4].map((num) => (
     <div
       key={num}
       className="w-1/4 h-64 bg-cover bg-center"
@@ -15,11 +16,20 @@ function EmptyComponent() {
   ))}
 </div>
 
-
-
-
-      <div className="bg-zinc-50 w-full h-16"></div>
-
+    <div className="flex items-center justify-between p-4 bg-zinc-50">
+      <span className="text-gray-700">Showing 1-20 results</span>
+      <div className="flex items-center space-x-2">
+        <button className="px-2 py-1 text-gray-700 bg-white border border-gray-300 rounded hover:bg-gray-200">{"<"}</button>
+        <button className="px-2 py-1 text-gray-700 bg-white border border-gray-300 rounded hover:bg-gray-200">{">"}</button>
+      </div>
+      <div className="flex items-center space-x-2">
+        <label htmlFor="sort" className="text-gray-700">Popularity</label>
+        <select id="sort" className="px-2 py-1 text-gray-700 bg-white border border-gray-300 rounded">
+          {/* Options would be populated here */}
+        </select>
+      </div>
+      <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">Filter</button>
+    </div>
 
     </div>
   )
