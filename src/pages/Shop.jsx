ae2dc0d22e14d7ddb8b32d8fe5b87e63fbcb2f27
Kerim <kerimcanak@gmail.com>
2024-08-07 05:47:07 +0300

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -6,32 +6,17 @@ function EmptyComponent() {
       <div className="bg-emerald-700 w-full h-16">
       </div>
       <Header/>
-      <div className="bg-zinc-50 w-full h-48 flex flex-col items-center justify-center gap-4 py-4 md:flex-row">
-         <div className="relative h-[150px]">
-           <img src="https://picsum.photos/400/300?random=1" alt="" className="w-full h-full object-cover"/>
-           <div className="absolute bottom-0 left-0 right-0 top-0 bg-black bg-opacity-50 flex items-center justify-center">
-            First
-          </div>
-        </div>
-         <div className="relative h-[150px]">
-           <img src="https://picsum.photos/400/300?random=2" alt="" className="w-full h-full object-cover"/>
-           <div className="absolute bottom-0 left-0 right-0 top-0 bg-black bg-opacity-50 flex items-center justify-center">
-            Second
-        </div>
-        </div>
-        <div className="relative h-[150px]">
-         <img src="https://picsum.photos/400/300?random=3" alt="" className="w-full h-full object-cover"/>
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-black bg-opacity-50 flex items-center justify-center">
-        Third
-        </div>
-    </div>
-        <div className="relative h-[150px]">
-    <img src="https://picsum.photos/400/300?random=4" alt="" className="w-full h-full object-cover"/>
-    <div className="absolute bottom-0 left-0 right-0 top-0 bg-black bg-opacity-50 flex items-center justify-center">
-      Fourth
-    </div>
-    </div>
-    </div>
+      <div className="bg-zinc-50 flex flex-col justify-around items-center p-4 gap-4 md:flex-row">  {[1, 2, 3, 4].map((num) => (
+    <div
+      key={num}
+      className="w-1/4 h-64 bg-cover bg-center"
+      style={{ backgroundImage: `url(https://picsum.photos/200?random=${num})` }}
+    ></div>
+  ))}
+</div>
+
+
+
 
       <div className="bg-zinc-50 w-full h-16"></div>
 
