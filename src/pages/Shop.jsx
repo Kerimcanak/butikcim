3e9ef5b95ae2704d85c1b5871eb11582d453f0a6
Kerim <kerimcanak@gmail.com>
2024-08-08 04:26:25 +0300

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,9 +8,19 @@ import LogoList from "../components/LogoList";
 function Shop() {
   return (
     <>
-      <div className="bg-emerald-700 w-full h-16 invisible md:visible">
-
-      </div>
+      <nav className="bg-emerald-700 p-4 flex items-center justify-between">
+        <div className="text-white font-bold">practice.dev</div>
+        <input
+          type="search"
+          placeholder="Search for challenges"
+          className="px-4 py-2 rounded-lg focus:outline-none"
+        />
+        <div>
+          <i className="fas fa-home text-white mx-2"></i>
+          <i className="fas fa-bell text-white mx-2"></i>
+          <i className="fas fa-user text-white mx-2"></i>
+        </div>
+      </nav>
       <Header/>
       <ShopContent/>
       <LogoList/>
