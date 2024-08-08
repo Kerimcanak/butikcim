02db558f51dc5fee843e998659bb936a45866b9e
Kerim <kerimcanak@gmail.com>
2024-08-08 04:31:31 +0300

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,17 +8,20 @@ import LogoList from "../components/LogoList";
 function Shop() {
   return (
     <>
-      <nav className="bg-emerald-700 p-4 flex items-center justify-between">
-        <div className="text-white font-bold">practice.dev</div>
-        <input
-          type="search"
-          placeholder="Search for challenges"
-          className="px-4 py-2 rounded-lg focus:outline-none"
-        />
+      <nav className="bg-emerald-700 p-4 flex items-center justify-between invisible md:visible">
+        <div className="text-white mr-4">
+          <i className="fas fa-phone-square-alt mr-2"></i>
+          (012) 345-6789
+        </div>
+        <div className="text-white font-bold">
+          <span>Follow us and get 50% off!</span>
+        </div>
+
         <div>
-          <i className="fas fa-home text-white mx-2"></i>
-          <i className="fas fa-bell text-white mx-2"></i>
-          <i className="fas fa-user text-white mx-2"></i>
+          <span className="text-white mr-4">Follow us:</span>
+          <i className="fab fa-facebook text-white mx-2"></i>
+          <i className="fab fa-twitter text-white mx-2"></i>
+          <i className="fab fa-instagram text-white mx-2"></i>
         </div>
       </nav>
       <Header/>
