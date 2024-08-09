09bf1a0a1ec275f1f084940c393cb058d96df3f6
Kerimcan AK <kerimcanak@gmail.com>
2024-08-09 10:53:09 +0300

    added logos to main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../layout/Header"
 import PageContent from "../layout/PageContent"
 import Footer from "../layout/Footer"
+import LogoList from "../components/LogoList"
 const HomePage = () => {
   return (
     <div className="overflow-hidden">
     <Header/>
+    <LogoList/>
     <PageContent/>
     <Footer/>
     </div>
