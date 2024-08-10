4cbf85b80f50d7d35edb626946fe23c66051fa37
Kerimcan AK <kerimcanak@gmail.com>
2024-08-11 01:03:16 +0300

    first division of the page

---
+++ src/layout/AboutusContent.jsx
@@ -0,0 +1,23 @@
+//tailwind css
+//let us use react router and links whenever we can
+//let us remember this is a component, a part of larger web app
+
+function AboutsContent () {
+    return (
+        <>
+        <div className="bg-white h-screen w-full flex justify-center items-center">
+            <div className="max-w-[1200px] w-full text-center">
+                <h1 className="text-4xl font-bold text-gray-800 mb-8">About Us</h1>
+                <p className="text-gray-600 mb-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fugiat quidem, quo quaerat quisquam, vero quod aspernatur possimus ea id exercitationem quis aliquid tempora beatae quidem.
+                </p>
+                <button className="bg-blue-500 text-white px-8 py-3 rounded-md">Learn More</button>
+            </div>
+        </div>
+        <ohmygod/>
+        <pleasework/>
+        </>
+    )
+}
+
+export default AboutsContent
